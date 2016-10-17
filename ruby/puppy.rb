5ba41bed8f24847444a16a1ecdbf47c53e57d6ff
Jeffrey Chan <jeffreychingchan@gmail.com>
2016-10-17 12:08:53 -0700

    add class in puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+SPECIES ------------------------
+chihuahua
+
+CHARACTERISTICS ----------------
+size: small
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+Furry: yes
+country of origin: Mexico
+
+
+BEHAVIOR -----------------------
+Jump
+bark
+walk
+poop 
+
